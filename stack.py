e41a9259febd36129ff918921d023a35d5fdbb97
Michael Kane <mediocrecomedy@gmail.com>
2017-02-04 12:33:46 -0800

    complete stack

--- stack.py
+++ stack.py
@@ -1,11 +1,20 @@
 #!python
 
+
+class Node(object):
+
+    def __init__(self, cargo):
+        self.next = None
+        self.cargo = cargo
+
+
 class Stack(object):
 
     def __init__(self, iterable=None):
         """Initialize this stack and push the given items, if any"""
         # TODO: initialize instance variables
-        pass
+        self.counter = 0
+        self.head = None
         if iterable:
             for item in iterable:
                 self.push(item)
@@ -16,27 +25,45 @@ class Stack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise"""
-        # TODO: check if empty
-        pass
+        return (self.counter == 0)
 
     def length(self):
         """Return the number of items in this stack"""
-        # TODO: count number of items
-        pass
+        return self.counter
 
     def peek(self):
         """Return the top item on this stack without removing it,
         or None if this stack is empty"""
-        # TODO: return top item, if any
-        pass
+        if self.counter != 0:
+            return self.head.cargo
+        else:
+            return None
 
     def push(self, item):
         """Push the given item onto this stack"""
-        # TODO: push given item
-        pass
+        new_node = Node(item)
+        if self.head is None:
+            self.head = new_node
+            self.counter = 1
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.counter += 1
+
 
     def pop(self):
         """Return the top item and remove it from this stack,
         or raise ValueError if this stack is empty"""
-        # TODO: pop top item, if any
-        pass
+        if self.is_empty():
+            raise ValueError("The Stack is empty!")
+        else:
+            # set aside cargo from head
+            # set head.next as head
+            # remove pointer from current head
+            #return cargo
+            old_head = self.head
+            new_head = old_head.next
+            self.head = new_head
+            old_head.next = None
+            self.counter -= 1
+            return old_head.cargo
